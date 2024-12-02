cded82a70c1c3624ed5944b01cd32ef49ec579d4
carballo91 <carballo.rafael91@gmail.com>
2024-12-01 22:23:01 -0500

    Add files via upload

---
+++ beavers_homes/static/beavers_homes/js/main.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function(){
+    let contact_message = document.getElementById("message");
+    setTimeout(function(){
+        contact_message.style.display = "none";
+    },4000)
+});
