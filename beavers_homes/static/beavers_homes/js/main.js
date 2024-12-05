efce523ff53d63eacf17892606b1c7afb4ec49e9
Rafael <carballo.rafael91@users.noreply.github.com>
2024-12-04 23:49:51 -0500

    Changed logic to fade out error message

--- beavers_homes/static/beavers_homes/js/main.js
+++ beavers_homes/static/beavers_homes/js/main.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function(){
     let contact_message = document.getElementById("message");
-    setTimeout(function(){
-        contact_message.style.display = "none";
-    },4000)
+    if (contact_message != null){
+        setTimeout(function(){
+            contact_message.style.display = "none";
+        },4000);
+    } 
 });
