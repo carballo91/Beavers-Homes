6a3f8dab2d27b4502baf2b0b7c19c1708ad4badd
Rafael <carballo.rafael91@users.noreply.github.com>
2025-08-16 02:03:46 -0400

    Updated site

--- beavers_homes/views.py
+++ beavers_homes/views.py
@@ -5,6 +5,7 @@ from django.core.mail import send_mail
 from django.conf import settings
 from django.urls import reverse
 import os
+from datetime import datetime
 
 # Create your views here.
 def index(request):
@@ -66,4 +67,49 @@ def contact(request):
             #return render(request, "beavers_homes/contact.html", {"form":ContactForm(),"message":"Thanks for contacting us!"})
     else:
         form = ContactForm()
-    return render(request, "beavers_homes/contact.html", {"form":form})
+    return render(request, "beavers_homes/contact.html", {"form":form,"TURNSTILE_SITE_KEY": settings.TURNSTILE_SITE_KEY})
+
+def sitemap(request):
+    """
+    Generates a sitemap.xml for search engines.
+    """
+
+    # List of static pages (named url pattern, change frequency, priority)
+    static_pages = [
+        ("index", "weekly", "1.0"),
+        ("about", "yearly", "0.8"),
+        ("gallery", "monthly", "0.8"),
+        ("contact", "yearly", "0.7"),
+    ]
+
+    lastmod = datetime.utcnow().date().isoformat()
+    xml_parts = [
+        '<?xml version="1.0" encoding="UTF-8"?>',
+        '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">',
+    ]
+
+    for name, changefreq, priority in static_pages:
+        loc = request.build_absolute_uri(reverse(name))
+        print(f"loc: {loc}")
+        xml_parts.append("  <url>")
+        xml_parts.append(f"    <loc>{loc}</loc>")
+        xml_parts.append(f"    <lastmod>{lastmod}</lastmod>")
+        xml_parts.append(f"    <changefreq>{changefreq}</changefreq>")
+        xml_parts.append(f"    <priority>{priority}</priority>")
+        xml_parts.append("  </url>")
+
+    xml_parts.append("</urlset>")
+
+    sitemap_xml = "\n".join(xml_parts)
+    return HttpResponse(sitemap_xml, content_type="application/xml")
+
+def robots_txt(request):
+    """
+    Serve robots.txt
+    """
+    lines = [
+        "User-Agent: *",
+        "Disallow: /admin/",
+        "Sitemap: https://www.beavershomesllc.com/sitemap.xml"
+    ]
+    return HttpResponse("\n".join(lines), content_type="text/plain")
